6f65733e9a9d7aa390369b0daacfade7d09872b3
Shubham Zungure <zungureshubham1@gmail.com>
2025-04-08 14:15:37 +0530

    added test code

--- 21March/Pattern1.java
+++ 21March/Pattern1.java
@@ -1,68 +1,14 @@
-class PrintPattern
-{
-	void printPattern()
-	{
-		for(int i = 1; i <= 5; i++)
-		{
-			
-			int num = 1;
-
-			for(int j = i; j <= 5; j++)
-			{
-				System.out.print(" ");
-			}
-			for(int j = 1; j <= i ; j=j+2)
-			{
-				char ch = 'A';
-				if(j == 1)
-				{
-					System.out.print("*");
-				}
-				else
-				{
-					System.out.print("*"+ch+(num)+"*");
-					num++;
-					ch++;
-				}				
-			}
-			System.out.println();
-		}
-	}
-
-	public static void main(String[] args)
-	{
-		PrintPattern p1 = new PrintPattern();
-		p1.printPattern();	
-	}
-}
-
-
-
-
-                         *
-			*A1*
-		       *A B2*
-                      *A   B3*
-                     *A B C D4*
-		      *A   B3*
-                       *A B2*
-                        *A1*
-			 *
-
-
-
-
 class Pattern1
 {
 	void printPattern()
 	{
 		int n = 4;
 		int num = 1;
-		for(int i = 0; i < n; i++)
+		for(int i = 1; i < n; i++)
 		{
 			System.out.print(" ");
 		}
-		System.out.println("*");
+		System.out.println(" *");
 		
 
 		for(int i = 1; i <= n; i++)
@@ -79,10 +25,6 @@ class Pattern1
 				if(j == 1 || j == i || i == n)
 				{
 					System.out.print((char)('A'+j-1));
-				}
-				else if(i == 3 && j == 3)
-				{
-					System.out.print('B');
 				}
 				else
 				{
@@ -108,13 +50,9 @@ class Pattern1
 
 			for(int j = 1; j <= i; j++)
 			{
-				if(j == 1 || j == i || i == n)
+				if(j == 1 || j == i)
 				{
 					System.out.print((char)('A' +j-1));
-				}
-				else if(i == 3 && j == 2)
-				{
-					System.out.print('B');
 				}
 				else
 				{
@@ -128,11 +66,11 @@ class Pattern1
 			System.out.println(num-- + "*");
 		}
 
-		for(int i = 0; i < n; i++)
+		for(int i = 1; i < n; i++)
 		{
 			System.out.print(" ");
 		}
-		System.out.println("*");
+		System.out.println(" *");
 	}
 
 	public static void main(String[] args)
@@ -141,14 +79,3 @@ class Pattern1
 		p1.printPattern();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
